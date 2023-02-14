5fc64dabf61df027e8073f5e697b4f5472a7bc9e
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-02-14 16:38:57 +0000

    1

--- hash.c
+++ hash.c
@@ -2,7 +2,7 @@
 unsigned long hash_key(const char* word){
   unsigned long val=0;
   for(i=0;i<strlen(word);i++){
-    val=  word[i] + FACTOR*val;
+    val=  word[i] + (FACTOR*val) % SIZE;
   };
   return  val % SIZE
 };
