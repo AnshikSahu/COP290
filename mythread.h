52c21891cdff959df450ca5acb24b96996a7fbd0
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-02-17 21:29:58 +0530

    Update mythread.h

--- mythread.h
+++ mythread.h
@@ -15,36 +15,24 @@ ucontext_t *main_context;
 
 void mythread_init(){
     ThreadList = list_new();
-    printf("Hello\n");
     main_context = (ucontext_t*)malloc(8192);
 }
 
 ucontext_t* mythread_create(void func(void*), void* arg){
     ucontext_t *context= (ucontext_t*)malloc(sizeof(ucontext_t));
-    printf("Hello");
     getcontext(context);
     (*context).uc_stack.ss_sp = malloc(STACK_SIZE);
     (*context).uc_stack.ss_size = STACK_SIZE;
     (*context).uc_link = main_context;
-    printf("Hello");
     makecontext(context, (void (*)(void))func,1,arg);
-    printf("Hello");
     list_add(ThreadList,context);
 
     return context;
 }
 
 void mythread_join(){
-    printf("Hello joining");
-    if(ThreadList->head == NULL){
-        printf("No threads to join");
-    }
-    else{
-        printf("Hello joining");
-    }
     getcontext(main_context);
     while (ThreadList->head != NULL) {
-        printf("Hello switching");
         swapcontext(main_context, ThreadList->head->data);
         list_rm(ThreadList,ThreadList->head);
         }
