79188df17002259601d6d7e6554d9c14105f9d9c
Anshik Sahu <97736240+AnshikSahu@users.noreply.github.com>
2023-02-18 19:11:46 +0530

    part2

---
+++ part2/list.h
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct list {
+	struct listentry* head;
+	struct listentry* tail;
+};
+struct listentry {
+	void *data;
+	struct listentry *prev;
+	struct listentry *next;
+};
+
+struct list* list_new() {
+    struct list* l =malloc(sizeof(struct list));
+    l->head = NULL;
+    l->tail = NULL;
+    return l;
+}
+
+struct listentry* list_add(struct list* l, void* data) {
+    struct listentry* e = malloc(sizeof(struct listentry));
+    e->data = data;
+    e->prev = NULL;
+    e->next = NULL;
+    if (l->head == NULL) {
+        l->head = e;
+        l->tail = e;
+    } else {
+        l->tail->next = e;
+        e->prev = l->tail;
+        l->tail = e;
+    }
+    return e;
+}
+
+void list_rm(struct list* l, struct listentry* e) {
+    if (e->prev == NULL) {
+        l->head = e->next;
+    } else {
+        e->prev->next = e->next;
+    }
+    if (e->next == NULL) {
+        l->tail = e->prev;
+    } else {
+        e->next->prev = e->prev;
+    }
+}
---
+++ part2/mythread.h
@@ -0,0 +1,86 @@
+#include "list.h"
+#define _XOPEN_SOURCE 600
+#include <stddef.h>
+#include <stdlib.h>
+#include <ucontext.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define STACK_SIZE 64000
+
+struct list *ThreadList;
+ucontext_t *main_context;
+
+void mythread_init(){
+    ThreadList = list_new();
+    printf("Hello\n");
+    main_context = (ucontext_t*)malloc(STACK_SIZE);
+}
+
+ucontext_t* mythread_create(void func(void*), void* arg){
+    ucontext_t *context= (ucontext_t*)malloc(STACK_SIZE);
+    getcontext(context);
+    (*context).uc_stack.ss_sp = malloc(STACK_SIZE);
+    (*context).uc_stack.ss_size = STACK_SIZE;
+    (*context).uc_link = main_context;
+    makecontext(context, (void (*)(void))func,1,arg);
+    list_add(ThreadList,context);
+
+    return context;
+}
+
+void mythread_join(){
+    printf("Hello joining");
+    if(ThreadList->head == NULL){
+        printf("No threads to join");
+    }
+    else{
+        printf("Hello joining");
+    }
+    getcontext(main_context);
+    while (ThreadList->head != NULL) {
+        printf("Hello switching");
+        swapcontext(main_context, ThreadList->head->data);
+        list_rm(ThreadList,ThreadList->head);
+        }
+    }
+
+void mythread_yield(){
+    if(ThreadList->head != NULL ){
+        if(ThreadList->head->next != NULL){
+    ucontext_t* prev_context = (ucontext_t*)malloc(STACK_SIZE);
+    prev_context = (ucontext_t*)ThreadList->head->data;
+     ucontext_t* next_context = (ucontext_t*)malloc(STACK_SIZE);
+    next_context = (ucontext_t*)ThreadList->head->next->data;
+    ThreadList->tail->next = ThreadList->head;
+    ThreadList->tail = ThreadList->head;
+    // struct listentry *l1=ThreadList->head->next;
+    list_rm(ThreadList,ThreadList->head);
+    ThreadList->tail->next = NULL;
+    // while(l1->next != NULL){
+    //     l1=l1->next;
+    // }
+    swapcontext(prev_context,next_context);
+    }}
+}
+
+struct lock {
+    ucontext_t* ctx;
+};
+
+struct lock* lock_new(){
+    struct lock* lk = (struct lock*)malloc(sizeof(struct lock));
+    return lk;
+}
+
+void lock_acquire(struct lock* lk){
+    while(lk->ctx != NULL){
+        mythread_yield();
+    }
+        lk->ctx = ThreadList->head->data;
+}
+
+int lock_release(struct lock* lk){
+    lk->ctx = NULL;
+        return 1;
+}
